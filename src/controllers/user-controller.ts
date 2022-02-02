e04f51f8e660c4970e1b19bd0de479764b88b410
misha <zinchuckmisha@ukr.net>
2022-02-02 21:57:53 +0300

    user-controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,17 @@
+import {Request, Response, NextFunction} from "express";
+const UserService = require('../services/user-service')
+
+class UserController{
+    async create(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        try {
+            const {name, email, password, confirmPassword} = req.body;
+            await UserService.create(name, email, password, confirmPassword);
+            // res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            // return res.json(userData)
+        } catch (e) {
+            next(e)
+        }
+    }
+}
+
+module.exports = new UserController();
